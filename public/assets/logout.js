a8d22d7b9a94dae9e6798eff86012bb29f859ae6
Edinei <galera.org@gmail.com>
2018-12-13 19:24:07 -0200

    logout feedback default

--- public/assets/logout.js
+++ public/assets/logout.js
@@ -1,12 +1,8 @@
 clearCache().then(d => {
     toast("Saindo...", "toast-warning");
     post('login', 'logout', function(g) {
-        if(g == 1) {
-            setTimeout(function () {
-                location.href = HOME + "login";
-            },700);
-        } else {
-            toast("Erro", "toast-error");
-        }
+        setTimeout(function () {
+            location.href = HOME + "login";
+        },700);
     });
 });
