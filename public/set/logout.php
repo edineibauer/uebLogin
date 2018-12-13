1f8bb0d634810d2cd97f9657851472da8190d3fa
Edinei <galera.org@gmail.com>
2018-12-13 18:30:42 -0200

    logout remove callback

--- public/set/logout.php
+++ public/set/logout.php
@@ -1,9 +1,3 @@
 <?php
 $login = new \Login\Login();
 $login->logOut();
-
-if($login->getResult() && !$login->getError()) {
-    echo json_encode(array("status" => 1, "mensagem" => $login->getResult()));
-} else if($login->getError()) {
-    echo json_encode(array("status" => 2, "mensagem" => $login->getError()));
-}
