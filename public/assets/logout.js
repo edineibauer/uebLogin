3a665ef63df4c62dc4d651cd2fe544457010136e
Edinei <galera.org@gmail.com>
2019-01-23 10:00:02 -0200

    logout check connection

--- public/assets/logout.js
+++ public/assets/logout.js
@@ -1,6 +1,13 @@
-toast("Saindo...", 3000, "toast-warning");
-clearCache().then(() => {
-    post('login', 'logout', function(g) {
-        location.href = HOME + "login";
-    });
-});
+post('route', 'internet', {}, function (g) {
+    if(g === 1) {
+        toast("Saindo...", 3000, "toast-warning");
+        clearCache().then(() => {
+            post('login', 'logout', function(g) {
+                location.href = HOME + "login";
+            });
+        });
+    } else {
+        setCookieAnonimo();
+        window.location = HOME;
+    }
+})
