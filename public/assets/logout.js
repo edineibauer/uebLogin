af3ded52397bb4c5de75679cb118c4402be781d6
Edinei <galera.org@gmail.com>
2019-01-31 15:01:57 -0200

    clear cache instead updatecache

--- public/assets/logout.js
+++ public/assets/logout.js
@@ -2,7 +2,7 @@ post('route', 'internet', {}, function (g) {
     if(g === 1) {
         toast("Saindo...", 3000, "toast-warning");
         post('login', 'logout', function(g) {
-            updateCacheLogin().then(() => {
+            clearCacheLogin().then(() => {
                 location.href = HOME + "login";
             });
         });
