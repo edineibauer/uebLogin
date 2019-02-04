6dcad04172d55082c45a9144aef2b44f51fed829
Edinei <galera.org@gmail.com>
2019-02-04 11:07:54 -0200

    só altera cookies se for edição do perfil

--- public/react/usuarios/update.js
+++ public/react/usuarios/update.js
@@ -1,6 +1,8 @@
-setCookie("nome", dados.nome);
-setCookie("imagem", typeof dados.imagem === "object" && typeof dados.imagem[0] !== "undefined" ? dados.imagem[0].image : "");
-setCookie("nome_usuario", slug(dados.nome));
-setCookie("email", dados.email);
-dashboardSidebarInfo();
-setSidebarInfo();
+if (dados.id == getCookie("id")) {
+    setCookie("nome", dados.nome);
+    setCookie("imagem", typeof dados.imagem === "object" && typeof dados.imagem[0] !== "undefined" ? dados.imagem[0].image : "");
+    setCookie("nome_usuario", slug(dados.nome));
+    setCookie("email", dados.email);
+    dashboardSidebarInfo();
+    setSidebarInfo();
+}
