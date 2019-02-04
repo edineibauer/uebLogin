d5d4ddc3ec39bfddea1514a48c4e7864aeb0688a
Edinei <galera.org@gmail.com>
2019-02-04 09:45:25 -0200

    react update perfil update dashboard info

--- public/react/usuarios/update.js
+++ public/react/usuarios/update.js
@@ -2,8 +2,5 @@ setCookie("nome", dados.nome);
 setCookie("imagem", typeof dados.imagem === "object" && typeof dados.imagem[0] !== "undefined" ? dados.imagem[0].image : "");
 setCookie("nome_usuario", slug(dados.nome));
 setCookie("email", dados.email);
-setCookie("setor", dados.setor);
-setCookie("status", dados.status);
-setCookie("nivel", dados.nivel);
 dashboardSidebarInfo();
 setSidebarInfo();
