2069c37f1c600c35b17bd4ea3c88c2da741b409c
Edinei <galera.org@gmail.com>
2019-02-04 16:32:52 -0200

    email e senha not null

--- public/set/login.php
+++ public/set/login.php
@@ -6,5 +6,12 @@ $dados['email'] = strip_tags(trim(filter_input(INPUT_POST, "email", FILTER_DEFAU
 $dados['password'] = trim(filter_input(INPUT_POST, "pass", FILTER_DEFAULT));
 $dados['recaptcha'] = filter_input(INPUT_POST, "recaptcha", FILTER_DEFAULT);
 
-$login = new Login($dados);
-$data['data'] = $login->getResult();
+if(!empty($dados['email']) && !empty($dados['password'])) {
+    $login = new Login($dados);
+    $data['data'] = $login->getResult();
+} else {
+    if(!empty($dados['email']))
+        $data['data'] = "Informe o Email";
+    else
+        $data['data'] = "Informe a Senha";
+}
