0467bffc1bff497c4f8c529dc9ec8e08880e3209
Edinei J. Bauer <galera.org@gmail.com>
2019-03-19 08:11:53 -0300

    logout

--- public/set/logout.php
+++ public/set/logout.php
@@ -1,5 +1,22 @@
 <?php
-use Login\Login;
 
-$login = new Login();
-$login->logOut();
+use Conn\TableCrud;
+
+if (isset($_SESSION['userlogin'])) {
+    if (!empty($_SESSION['userlogin']['token']) || (isset($_COOKIE['token']) && $_COOKIE['token'] !== "0")) {
+        $t = !empty($_SESSION['userlogin']['token']) ? $_SESSION['userlogin']['token'] : $_COOKIE['token'];
+        $token = new TableCrud("usuarios");
+        $token->load("token", $t);
+        if ($token->exist()) {
+            $token->setDados(["token" => null, "token_expira" => null]);
+            $token->save();
+        }
+    }
+}
+session_unset();
+
+$this->setCookie("token", 0, -1);
+$this->setCookie("id", 0, -1);
+$this->setCookie("nome", 0, -1);
+$this->setCookie("imagem", 0, -1);
+$this->setCookie("setor", 0, -1);
--- public/src/Login/Login.php
+++ public/src/Login/Login.php
@@ -196,28 +196,6 @@ class Login
         }
     }
 
-    public function logOut()
-    {
-        if (isset($_SESSION['userlogin'])) {
-            if (!empty($_SESSION['userlogin']['token']) || (isset($_COOKIE['token']) && $_COOKIE['token'] !== "0")) {
-                $t = !empty($_SESSION['userlogin']['token']) ? $_SESSION['userlogin']['token'] : $_COOKIE['token'];
-                $token = new TableCrud("usuarios");
-                $token->load("token", $t);
-                if ($token->exist()) {
-                    $token->setDados(["token" => null, "token_expira" => null]);
-                    $token->save();
-                }
-            }
-        }
-        session_unset();
-
-        $this->setCookie("token", 0, -1);
-        $this->setCookie("id", 0, -1);
-        $this->setCookie("nome", 0, -1);
-        $this->setCookie("imagem", 0, -1);
-        $this->setCookie("setor", 0, -1);
-    }
-
     /**
      * Seta dados de um usuário como login de acesso
      * @param array $usuario
