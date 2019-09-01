5dfeae8be49fdd692f526d93139bce261a8add9c
Edinei J. Bauer <galera.org@gmail.com>
2019-08-31 22:13:54 -0300

    space fixed

--- public/view/login.php
+++ public/view/login.php
@@ -1,5 +1,5 @@
 <?php
-if(!empty($_SESSION['userlogin']['token'])) {
+if (!empty($_SESSION['userlogin']['token'])) {
     $data = ["response" => 3, "data" => HOME + "dashboard", "error" => []];
 } else {
     ob_start();
@@ -10,29 +10,31 @@ if(!empty($_SESSION['userlogin']['token'])) {
                  style='height: 60px;float: initial;margin:initial'>
         </div>
         <div class='container align-center upper panel color-text-grey'>área restrita <?= SITENAME ?></div>
-        <div class="row z-depth-2 color-white" id="login-card">
-            <div class="panel">
+        <div class="row padding-medium">
+            <div class="row z-depth-2 color-white" id="login-card">
                 <div class="panel">
-                    <label class="row">
-                        <span>Email</span>
-                        <input id="emaillog" type="email" class="font-light font-large">
-                    </label>
-                    <label class="row">
-                        <span>Senha</span>
-                        <input id="passlog" type="password" class="font-light font-large">
-                    </label>
+                    <div class="panel">
+                        <label class="row">
+                            <span>Email</span>
+                            <input id="emaillog" type="email" class="font-light font-large">
+                        </label>
+                        <label class="row">
+                            <span>Senha</span>
+                            <input id="passlog" type="password" class="font-light font-large">
+                        </label>
 
-                    <?php if (defined("RECAPTCHASITE")) { ?>
-                        <div class="container">
-                            <div class="g-recaptcha" data-sitekey="<?= RECAPTCHASITE ?>"></div>
-                            <br>
-                        </div>
-                        <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
+                        <?php if (defined("RECAPTCHASITE")) { ?>
+                            <div class="container">
+                                <div class="g-recaptcha" data-sitekey="<?= RECAPTCHASITE ?>"></div>
+                                <br>
+                            </div>
+                            <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
 
-                    <?php } else { ?>
+                        <?php } else { ?>
 
-                    <input type="hidden" id="g-recaptcha-response"/>
-                    <?php } ?>
+                        <input type="hidden" id="g-recaptcha-response"/>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -48,7 +50,7 @@ if(!empty($_SESSION['userlogin']['token'])) {
         <div class="row clearfix"><br></div>
 
         <div class="row upper color-text-grey font-small padding-medium padding-4">
-            <!--<a href="<?/*= defined('HOME') ? HOME : "" */ ?>cadastro-usuario"
+            <!--<a href="<? /*= defined('HOME') ? HOME : "" */ ?>cadastro-usuario"
                class="left btn color-white color-text-grey hover-opacity-off opacity" style="text-decoration: none">
                 Cadastre-se
             </a>-->
