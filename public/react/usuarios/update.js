42a559262288035d1fa680667f0a6add359912cd
Edinei J. Bauer <galera.org@gmail.com>
2019-10-27 20:58:31 -0300

    fix localStorage image data

--- public/react/usuarios/update.js
+++ public/react/usuarios/update.js
@@ -1,6 +1,8 @@
 if (dados.id == getCookie("id")) {
-    dados.imagem = JSON.parse(dados.imagem)[0];
-    delete dados.imagem.preview;
+    if(dados.imagem !== "" && dados.imagem !== "null") {
+        dados.imagem = JSON.parse(dados.imagem)[0];
+        delete dados.imagem.preview;
+    }
     setCookieUser(dados).then(() => {
         dashboardSidebarInfo();
     })
