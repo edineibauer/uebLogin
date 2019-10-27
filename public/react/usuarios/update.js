01e9843534f51b55e71e9f4a3ddc31cd4a5d2810
Edinei J. Bauer <galera.org@gmail.com>
2019-10-27 15:37:20 -0300

    react update user

--- public/react/usuarios/update.js
+++ public/react/usuarios/update.js
@@ -1,10 +1,12 @@
 if (dados.id == getCookie("id")) {
     $.each(dados, function (i, e) {
-        if(typeof e === "object")
-            e = JSON.stringify(e);
+        if(i !== "id_old" && i !== "db_action" && i !== "db_status") {
+            if (typeof e === "object")
+                e = JSON.stringify(e);
 
-        setCookie(i, e);
-        localStorage.setItem(i, e);
+            setCookie(i, e);
+            localStorage.setItem(i, e);
+        }
     });
     dashboardSidebarInfo();
     menuDashboard();
