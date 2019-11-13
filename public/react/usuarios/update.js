d767a6e28f43329099fb7ea2d2c42a8717b99f36
Edinei J. Bauer <galera.org@gmail.com>
2019-11-12 22:08:25 -0300

    update react image and name logic

--- public/react/usuarios/update.js
+++ public/react/usuarios/update.js
@@ -1,13 +1,22 @@
 if (dados.id == getCookie("id")) {
-    if(dados.imagem !== "" && dados.imagem !== "null") {
+    if(!isEmpty(dados.imagem) && dados.imagem !== "null") {
         dados.imagem = JSON.parse(dados.imagem)[0];
         delete dados.imagem.preview;
+
+        if (typeof dados.imagem === "object")
+            dados.imagem = JSON.stringify(dados.imagem);
+
+        setCookie("imagem", dados.imagem);
+        localStorage.setItem("imagem", dados.imagem);
+    } else {
+        setCookie("imagem", "");
+        localStorage.setItem("imagem", "");
     }
 
-    if(dados.setor === "" || dados.setor === "null" || dados.setor === null)
-        dados.setor = "admin";
+    setCookie("nome", dados.nome);
+    localStorage.setItem("nome", dados.nome);
 
-    setCookieUser(dados).then(() => {
+    menuHeader().then(() => {
         dashboardSidebarInfo();
     })
 }
