98280047d7ae94b55fef62504bae706bb4cf701a
Edinei J. Bauer <galera.org@gmail.com>
2019-12-20 11:36:10 -0300

    api login

---
+++ public/api/login.php
@@ -0,0 +1,17 @@
+<?php
+
+use Login\Login;
+
+$dados = $_POST;
+$dados['user'] = strip_tags(trim($dados['user']));
+$dados['password'] = trim($dados["password"]);
+
+if(!empty($dados['user']) && !empty($dados['password'])) {
+    $login = new Login($dados);
+    $data['data'] = $login->getResult();
+} else {
+    if(!empty($dados['user']))
+        $data['data'] = "Informe o Usuário";
+    else
+        $data['data'] = "Informe a Senha";
+}
