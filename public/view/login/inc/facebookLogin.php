e207da426dfabe5336eef3f99cdc4b7b3b53c21e
Edinei J. Bauer <galera.org@gmail.com>
2020-06-21 10:27:40 -0300

    login social check if entity existe

--- public/view/login/inc/facebookLogin.php
+++ public/view/login/inc/facebookLogin.php
@@ -1,97 +1,101 @@
 <?php
 if (defined("FACEBOOKAPLICATIONID") && !empty(FACEBOOKAPLICATIONID)) {
-    ?>
-    <script>
-        /**
-         * Login with the user facebook
-         */
-        async function loginUserFBBase(token) {
-            let user = await getUserFB();
-            user.image = user.picture.data.url;
-            user.token = token;
-            delete(user.picture);
+    if (defined('FACEBOOKENTITY') && !empty(FACEBOOKENTITY) && file_exists(PATH_HOME . "entity/cache/" . FACEBOOKENTITY)) {
+        ?>
+        <script>
+            /**
+             * Login with the user facebook
+             */
+            async function loginUserFBBase(token) {
+                let user = await getUserFB();
+                user.image = user.picture.data.url;
+                user.token = token;
+                delete (user.picture);
 
-            if(typeof loginFacebook === "function") {
-                loginFacebook(user);
-            } else {
-                toast("Admin! Implemente a função `loginFacebook(profile)` em seu código para fazer algo com os dados retornados!", 10000, "toast-warning");
-                console.log("Admin! Implemente a função `loginFacebook(profile)` em seu código para fazer algo com os dados retornados!", user);
+                if (typeof loginFacebook === "function") {
+                    loginFacebook(user);
+                } else {
+                    toast("Admin! Implemente a função `loginFacebook(profile)` em seu código para fazer algo com os dados retornados!", 10000, "toast-warning");
+                    console.log("Admin! Implemente a função `loginFacebook(profile)` em seu código para fazer algo com os dados retornados!", user);
+                }
             }
-        }
 
-        /**
-         * Get the facebook perfil
-         */
-        async function getUserFB() {
-            return new Promise(s => {
-                FB.api(
-                    '/me',
-                    'GET',
-                    {"fields":"id,name,picture{url},email"},
-                    function(response) {
-                        s(response);
-                    }
-                );
-            });
-        }
-
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '<?=FACEBOOKAPLICATIONID?>',
-                cookie: true,
-                xfbml: true,
-                version: '<?=defined('FACEBOOKVERSION') && !empty(FACEBOOKVERSION) ? FACEBOOKVERSION : "v7.0"?>'
-            });
-
-            $(".facebook-login").off("click").on("click", function () {
-                FB.getLoginStatus(function(response) {
-                    if (response.status === 'connected' && response.authResponse) {
-                        toast("acessando...", "toast-success");
-                        loginUserFBBase(response.authResponse.accessToken);
-                    } else {
-                        FB.login(function(response) {
-                            if (response.authResponse)
-                                loginUserFBBase(response.authResponse.accessToken);
-                        });
-                    }
+            /**
+             * Get the facebook perfil
+             */
+            async function getUserFB() {
+                return new Promise(s => {
+                    FB.api(
+                        '/me',
+                        'GET',
+                        {"fields": "id,name,picture{url},email"},
+                        function (response) {
+                            s(response);
+                        }
+                    );
                 });
-            });
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
             }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
 
-    </script>
+            window.fbAsyncInit = function () {
+                FB.init({
+                    appId: '<?=FACEBOOKAPLICATIONID?>',
+                    cookie: true,
+                    xfbml: true,
+                    version: '<?=defined('FACEBOOKVERSION') && !empty(FACEBOOKVERSION) ? FACEBOOKVERSION : "v7.0"?>'
+                });
 
-    <button class="facebook-login">
-        facebook login
-    </button>
+                $(".facebook-login").off("click").on("click", function () {
+                    FB.getLoginStatus(function (response) {
+                        if (response.status === 'connected' && response.authResponse) {
+                            toast("acessando...", "toast-success");
+                            loginUserFBBase(response.authResponse.accessToken);
+                        } else {
+                            FB.login(function (response) {
+                                if (response.authResponse)
+                                    loginUserFBBase(response.authResponse.accessToken);
+                            });
+                        }
+                    });
+                });
+            };
 
-    <style>
-        .facebook-login {
-            background-image: url("<?=HOME . VENDOR?>login/public/assets/img/facebook.png");
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: left center;
-            background-color: #1877f2;
-            height: 40px;
-            color: #ffffff;
-            box-shadow: 0 2px 4px 0 rgba(0,0,0,.25);
-            border: none;
-            border-radius: 4px!important;
-            font-size: 13px!important;
-            width: 160px;
-            float: right;
-            padding-left: 30px!important;
-        }
-    </style>
-    <?php
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
+        </script>
+
+        <button class="facebook-login">
+            facebook login
+        </button>
+
+        <style>
+            .facebook-login {
+                background-image: url("<?=HOME . VENDOR?>login/public/assets/img/facebook.png");
+                background-size: contain;
+                background-repeat: no-repeat;
+                background-position: left center;
+                background-color: #1877f2;
+                height: 40px;
+                color: #ffffff;
+                box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .25);
+                border: none;
+                border-radius: 4px !important;
+                font-size: 13px !important;
+                width: 160px;
+                float: right;
+                padding-left: 30px !important;
+            }
+        </style>
+        <?php
+    } else {
+        echo 'toast("facebook login: Entidade `' . FACEBOOKENTITY . '` não existe", 4000, "toast-error");';
+    }
 }
--- public/view/login/inc/googleLogin.php
+++ public/view/login/inc/googleLogin.php
@@ -1,42 +1,47 @@
 <?php
-if (defined("GOOGLELOGINCLIENTID") && !empty(GOOGLELOGINCLIENTID)) { ?>
-    <div class="g-signin2" data-onsuccess="loginUserGoogleBase"></div>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id" content="<?=GOOGLELOGINCLIENTID?>">
-    <script>
-        function loginUserGoogleBase(googleUser) {
-            let profile = googleUser.getBasicProfile();
-            let user = {
-                id: profile.getId(),
-                name: profile.getName(),
-                email: profile.getEmail(),
-                image: profile.getImageUrl(),
-                token: googleUser.wc.id_token
+if (defined("GOOGLELOGINCLIENTID") && !empty(GOOGLELOGINCLIENTID)) {
+    if (defined('GOOGLEENTITY') && !empty(GOOGLEENTITY) && file_exists(PATH_HOME . "entity/cache/" . GOOGLEENTITY)) {
+        ?>
+        <div class="g-signin2" data-onsuccess="loginUserGoogleBase"></div>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <meta name="google-signin-client_id" content="<?= GOOGLELOGINCLIENTID ?>">
+        <script>
+            function loginUserGoogleBase(googleUser) {
+                let profile = googleUser.getBasicProfile();
+                let user = {
+                    id: profile.getId(),
+                    name: profile.getName(),
+                    email: profile.getEmail(),
+                    image: profile.getImageUrl(),
+                    token: googleUser.wc.id_token
+                }
+                if (typeof loginGoogle === "function") {
+                    loginGoogle(user);
+                } else {
+                    toast("Admin! Implemente a função `loginGoogle(profile)` em seu código para fazer algo com os dados retornados!", 10000, "toast-warning")
+                    console.log("Admin! Implemente a função `loginGoogle(profile)` em seu código para fazer algo com os dados retornados!", user);
+                }
+                /**
+                 * after work with the data user, logout
+                 */
+                gapi.auth2.getAuthInstance().signOut();
             }
-            if (typeof loginGoogle === "function") {
-                loginGoogle(user);
-            } else {
-                toast("Admin! Implemente a função `loginGoogle(profile)` em seu código para fazer algo com os dados retornados!", 10000, "toast-warning")
-                console.log("Admin! Implemente a função `loginGoogle(profile)` em seu código para fazer algo com os dados retornados!", user);
+        </script>
+        <style>
+            .g-signin2 > .abcRioButton {
+                height: 40px !important;
+                padding: 3px 5px;
+                width: 160px !important;
+                border-radius: 3px;
             }
-            /**
-             * after work with the data user, logout
-             */
-            gapi.auth2.getAuthInstance().signOut();
-        }
-    </script>
-    <style>
-        .g-signin2 > .abcRioButton {
-            height: 40px!important;
-            padding: 3px 5px;
-            width: 160px!important;
-            border-radius: 3px;
-        }
 
-        .g-signin2 {
-            float: left;
-            margin-bottom: 10px
-        }
-    </style>
-    <?php
+            .g-signin2 {
+                float: left;
+                margin-bottom: 10px
+            }
+        </style>
+        <?php
+    } else {
+        echo 'toast("google login: Entidade `' . GOOGLEENTITY . '` não existe", 4000, "toast-error");';
+    }
 }
