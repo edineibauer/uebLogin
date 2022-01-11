d06f29c0eb9e06a9c36a8d63ba6ccb73a118c579
edineijacobbauer <edineibauer@gmail.com>
2022-01-11 14:35:09 -0300

    not create log on login and clear login attempt all month

---
+++ public/cron/clearLoginAttempt.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Primeiro dia do mês na primeira hora exclui todas as tentativas de login registradas
+ */
+if(date("d H:i") === "01 01:00") {
+    $del = new \Conn\Delete();
+    $del->exeDelete("login_attempt", "WHERE id > 0");
+}
