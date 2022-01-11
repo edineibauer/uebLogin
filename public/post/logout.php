97261d21192338cf0f2c9bffa3f966a15a53f406
edineijacobbauer <edineibauer@gmail.com>
2022-01-11 15:59:47 -0300

    logout not delete userCache

--- public/post/logout.php
+++ public/post/logout.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * Exclui todo o cache
- */
-if(file_exists(PATH_HOME . "_cdn/userSSE/" . $_SESSION['userlogin']['id']))
-    \Helpers\Helper::recurseDelete(PATH_HOME . "_cdn/userSSE/" . $_SESSION['userlogin']['id']);
-
-/**
  * Read table token and remove toke access
  */
 $del = new \Conn\Delete();
